4e6e9a52e4ae9a02ba1f164a2be36a2940b3b4ac
avijeet108 <kumaravijeet108@gamil.com>
2022-10-15 00:47:06 +0530

    Working on Authentication

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:meetify/utils/colors.dart';
 
 class CustomButton extends StatelessWidget {
   final String text;
+  final VoidCallback onPressed;
 
-  CustomButton({required this.text});
+  CustomButton({required this.text, required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {},
-      child: Text(text),
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 17),
+        ),
+        style: ElevatedButton.styleFrom(
+            primary: buttonColor,
+            minimumSize: const Size(double.infinity, 50),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+              side: const BorderSide(color: buttonColor),
+            )),
+      ),
     );
   }
 }
